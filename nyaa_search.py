5497409b27a77ae888dcc067d3872f4debdeb7a5
Pun <puncedy@gmail.com>
2025-02-12 14:42:36 +0800

    create new script - Search items on Nyaa

---
+++ nyaa_search.py
@@ -0,0 +1,9 @@
+
+# TODO Open {FOLDER}\search.txt and parse the search terms.
+# TODO use Selenium, open Chromium and open nyaa.si
+# TODO search for the search terms
+# Be sure to create a crawl delay of 5 seconds to respect the Robots.txt
+# TODO wait for the search results to load
+# TODO create Criteria for Best Match, Ignore Low Seed Results
+# TODO parse the search results, grab the magnet and torrent link of the best match.
+# TODO save the search results to a .csv file
