60ca4a2737eacc5f4ea8bfd3ac77c0d7d15bdd22
Pun <puncedy@gmail.com>
2025-02-18 15:24:04 +0800

    added new function: Download Images.
    Only works on Linux because it uses wget
    
    alternatives could be used but I'm only developing this for myself

--- yatta_new_novels.py
+++ yatta_new_novels.py
@@ -3,12 +3,35 @@ import requests
 from datetime import datetime
 import pandas as pd
 import os
+import subprocess
+import time
 
 
 #Link to Yatta Tachi Resources. We're looking for the first link that points to the novel and manga releases for the month.
 YATTA = "https://yattatachi.com/category/resources"
 FOLDER = "YattaTachi" # Change this to your preferred folder name, only using that because that's what the folder name I used was
 
+def make_folders():
+    if not os.path.exists("images"):
+        os.makedirs("images")
+    if not os.path.exists(FOLDER):
+        os.makedirs(FOLDER)
+
+
+def save_img(book_img_link, book_title):
+    print("[INFO] Sleeping for 5 seconds to respect robots.txt")
+    time.sleep(5)
+    extension = os.path.splitext(book_img_link)[1]
+    filename = os.path.join("images", f"{book_title}{extension}")
+    print("[INFO] Saving image: " + filename)
+    forbidden_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
+    safe_filename = ''.join(c if c not in forbidden_chars else '_' for c in book_title)
+    filename = os.path.join("images", f"{safe_filename}{extension}")
+    cmd = ['wget', '-O', filename, book_img_link]
+    subprocess.Popen(cmd).communicate()
+    
+    
+    
 
 def tag_releases(title:str)->str: 
     if title.__contains__("Azuki") and title.__contains__("Chapter") or title.__contains__("NOOK Edition") and title.__contains__("#"):
@@ -38,6 +61,7 @@ def clean_for_search(title:str)->str:
 
     return title.translate(str.maketrans("","",":.()&,[]"))
     
+make_folders()
 
 try:
     print("[INFO] Opening Yatta Tachi Resources")
@@ -78,6 +102,9 @@ book_info = {}
 nook_releases = {}
 individual_chapters = {}
 search_titles = ["===="+book_release_post+"===="] # A way to identify which post we're looking at  
+
+
+
 for book in book_releases:
     # print(book)
     # Grab Release Date, format it to YYYY-MM-DD
@@ -87,6 +114,7 @@ for book in book_releases:
     formatted_date = date_object.strftime("%Y-%m-%d")
     # print(book_release_date)
     # print(formatted_date)
+    book_img_link = str(book.find("div", class_="release-img")).split("url(")[1].split(")")[0]
     book_title = book.find("a", class_="release-link").text
     # print(f"[INFO] Processing Book Release {book_title}")
     book_tag = tag_releases(book_title)
@@ -106,6 +134,7 @@ for book in book_releases:
     if book_tag == "None":
         search_titles.append(clean_title)
         book_info[book_title] = book_info_dict
+        save_img(book_img_link, book_title)
     elif book_tag == "NOOK Edition Release":
         nook_releases[book_title] = book_info_dict
     elif book_tag == "Individual Chapter Release":
@@ -120,8 +149,7 @@ filename = book_release_post.replace("/","").replace(" ","_")
 nook_file = "Nook_Edition_" + filename
 ind_chap_file = "Individual_Chapters_" + filename
 
-if not os.path.exists(FOLDER):
-    os.makedirs(FOLDER)
+
     
 
 with open(f"{FOLDER}/search.txt", "a") as file:
@@ -130,6 +158,7 @@ with open(f"{FOLDER}/search.txt", "a") as file:
 df.to_csv(f"{FOLDER}/{filename}.csv", index=False, mode='w')
 nook_df.to_csv(f"{FOLDER}/{nook_file}.csv", index=False, mode='w')
 ind_chap_df.to_csv(f"{FOLDER}/{ind_chap_file}.csv", index=False, mode='w')
+print("[INFO] Done, files are in the folder: " + FOLDER)
 
 
 
